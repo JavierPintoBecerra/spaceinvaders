6a6f99ca8112de57299d7ac01024dd51892e1e40
JavierPinto <javier.pinto.becerra@gmail.com>
2024-10-15 11:49:05 -0500

    Move the space ship

--- app.js
+++ app.js
@@ -21,15 +21,35 @@
         let result = 0;
         let direction = 1;
         let invaderId;
-
-        const invader = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-            30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,]; 
-
+        // Invader positions
+        const invader = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+            30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,]; 
+        //Renders invaders on game-board
         invader.forEach((invader) => {
-            cells[invadersPosition + invader].classList.add('invader');  
+            cells[invadersPosition + invader].classList.add('invader');    // You can remove invaderPosition from the brackets and it will work.
         });
 
+        // Renders ship on game-board
+        cells[shipPosition].classList.add('ship');
 
+        // Move the ship
+
+        function moveShip(event) {
+            cells[shipPosition].classList.remove('ship');
+            switch (event.key) {
+                case 'ArrowLeft':
+                    if (shipPosition % cellsWidth !== 0) shipPosition -= 1;
+                    break;
+                case 'ArrowRight':
+                    if (shipPosition % cellsWidth < cellsWidth - 1) shipPosition += 1;
+                    break;
+            }
+            // Remove ship
+            cells[shipPosition].classList.remove('ship');
+            // Add ship
+            cells[shipPosition].classList.add('ship');
+        }
+        document.addEventListener('keydown', moveShip);
 
     })
